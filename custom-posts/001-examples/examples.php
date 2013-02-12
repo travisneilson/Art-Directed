3b11ed17487903674a18f8a117fcc090fa7e0980
travisneilson <travisneilson@gmail.com>
2013-02-12 14:38:03 -0800

    Copy editing and link checking the first example post. Looks great now.

--- custom-posts/001-examples/examples.php
+++ custom-posts/001-examples/examples.php
@@ -22,7 +22,7 @@
         <h2>Peep Code</h2>        
         <p>Peepcode is a learning site. They record web professionals working and explaining their work and then sell the videos. Some really good stuff, but what we are talking about is the blog. While the main portion of the site is definatly a business the blog is beautiful, informative and at times, personal.</p>
         
-        <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+        <p><a href="http://peepcode.com/blog/2010/rethinking-rails-3-routes" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
         
         <ul class="inside-switch">
           <li class="prev">&#9664; Previous Blog</li>
@@ -30,7 +30,7 @@
         </ul>
       </div>
     </div>
-    <a href="" class="screen">
+    <a href="http://peepcode.com/blog/2010/rethinking-rails-3-routes" target="_blank" class="screen">
       <h3>Rethinking Rails 3 Controllers and Routes</h3>
       <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/peep-code.gif" style="width: 460px; height: 2732px;" alt="" />
     </a>
@@ -43,9 +43,9 @@
       <h2>Dustin Curtis</h2>        
       <p>Dustin Curtis is the self proclaimed villian.</p>
       
-      <p>He recently launched a blogging platform called SVBTLE, and supringly enough, it is the exact opposite of an Art Directed expereance. At one point Dustins Art-Directed blog was the poster-child for all art directed blogs. He's a smart guy, these posts are worth reading.</p>
+      <p>He recently launched a blogging platform called SVBTLE, and supringly enough, it is the exact opposite of an Art Directed experience. At one point Dustin's art-directed blog was the poster-child for all art-directed blogs. He's a smart guy, these posts are worth reading.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://www.dustincurtis.com/mirror_neuron.html" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -53,7 +53,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://www.dustincurtis.com/mirror_neuron.html" target="_blank" class="screen">
     <h3>The Monkey and the Thought Neuron</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/dustin-curtis.jpg" style="width: 460px; height: 1846px;" alt="" />
   </a>
@@ -68,7 +68,7 @@
       
       <p>If you are a fan of seeing what trendsetters in web style are doing, keep your tab open on this blog.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://v2.desandro.com/articles/do-make-create/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -76,7 +76,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://v2.desandro.com/articles/do-make-create/" target="_blank" class="screen">
     <h3>Do, Make, Create.</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/david-desandro.png" style="width: 460px; height: 794px;" alt="" />
   </a>
@@ -89,7 +89,7 @@
       <h2>72 Ave</h2>        
       <p>Jamie Calabro wrote a few very interesting and thoughtful posts in his blog.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://72ave.com/avoid-the-crap/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -97,7 +97,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://72ave.com/avoid-the-crap/" target="_blank" class="screen">
     <h3>Please, Create a Unique Site and Avoide the Crap</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/72ave.png" style="width: 460px; height: 840px;" alt="" />
   </a>
@@ -110,7 +110,7 @@
       <h2>Gregory Wood</h2>        
       <p>English designer Gregory Wood writes about everthing from zombie attacks to music and type reviews.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://journal.gregorywood.co.uk/entries/top-5-slab-serifs.php" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -118,7 +118,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="ttp://journal.gregorywood.co.uk/entries/top-5-slab-serifs.php" target="_blank" class="screen">
     <h3>Top 5 Slab Serifs</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/greg-wood.png" style="width: 460px; height: 895px;" alt="" />
   </a>
@@ -131,7 +131,7 @@
       <h2>I Love Typography</h2>        
       <p>More than a personal blog. This is a tome dedicated to hardcore type lovers. Every so often a post will be designed with unique features. As should go without saying for a blog about type, this is one beautiful website.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://ilovetypography.com/2012/08/24/condensed-typeface-design-program/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -139,7 +139,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://ilovetypography.com/2012/08/24/condensed-typeface-design-program/" target="_blank" class="screen">
     <h3> Condensed Typeface Design Program</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/ilt.jpg" style="width: 460px; height: 3368px;" alt="" />
   </a>
@@ -150,9 +150,9 @@
   <div class="description-wrap">
     <div class="description">
       <h2>Travis Neilson</h2>        
-      <p>Travis is the creator of the <a href="">Art Directed wordpress theme framework</a>. His own blog is super awesome, check it out.</p>
+      <p>Travis is the creator of the <a href="https://github.com/travisneilson/Art-Directed">Art Directed wordpress theme framework</a>. His own blog is super awesome, check it out.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://www.travisneilson.com/nyc/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -160,7 +160,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://www.travisneilson.com/nyc/" target="_blank" class="screen">
     <h3>NYC</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/travis-neilson.png" style="width: 460px; height: 1179px;" alt="" />
   </a>
@@ -174,7 +174,7 @@
       <h2>The Bold Italic</h2>        
       <p>The Bold Italic doesnt stray too far in terms of design, but they have awesome illustrative headers and great photography. The articles cover life in San Francisco, and all the strange nuance that this entails.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://www.thebolditalic.com/schelj/stories/2130-the-long-way-down" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -182,7 +182,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://www.thebolditalic.com/schelj/stories/2130-the-long-way-down" target="_blank" class="screen">
     <h3>The Long Way Down</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/bold-italic.jpg" style="width: 460px; height: 2347px;" alt="" />
   </a>
@@ -197,7 +197,7 @@
       <h2>Trent Walton</h2>        
       <p>Trent is awesome. His blog is not only great to look at, but a constant source of knowledge. Get the latest on css techniques and read up some well thought out personal articles.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://trentwalton.com/2012/03/06/into-the-woods/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -205,7 +205,7 @@
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://trentwalton.com/2012/03/06/into-the-woods/" target="_blank" class="screen">
     <h3>Into The Woods</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/trent-walton.jpg" style="width: 460px; height: 1055px;" alt="" />
   </a>
@@ -220,15 +220,15 @@
       <h2>Grain &amp; Gram</h2>        
       <p>G&amp;G spends a lot of time gathering photos and making videos to accompany their custom designed interviews of makers. Artists, musicians, printers, carpenters and more. This site is beautiful, inspiring, and interesting.</p>
       
-      <p><a href="#" class="visit">&#10095; Visit this Blog</a></p>
+      <p><a href="http://grainandgram.com/jasongregory/" target="_blank" class="visit">&#10095; Visit this Blog</a></p>
       
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
-        <li class="next">Conclusion &#9654;</li>
+        <li class="next">Next Blog  &#9654;</li>
       </ul>
     </div>
   </div>
-  <a href="" class="screen">
+  <a href="http://grainandgram.com/jasongregory/" target="_blank" class="screen">
     <h3>Jason Gregory</h3>
     <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/grain-gram.jpg" style="width: 460px; height: 3357px;" alt="" />
   </a>
@@ -239,10 +239,32 @@
 <div class="site">
   <div class="description-wrap">
     <div class="description">
-      <h2>Okay!</h2>        
+      <h2>Smashing Article</h2>        
+      <p>Smashing is not normally an art-directed blog, but made an exception here, when explaining the then-trend. This insightful article by Paddy Donnelly makes a good case for uniquely designed entries on your blog, and coins the term "blogazine."</p>
+      
+      <p><a href="http://www.smashingmagazine.com/the-death-of-the-blog-post/" target="_blank" class="visit">&#10095; Visit this Article</a></p>
+      
+      <ul class="inside-switch">
+        <li class="prev">&#9664; Previous Blog</li>
+        <li class="next">Conclusion &#9654;</li>
+      </ul>
+    </div>
+  </div>
+  <a href="http://www.smashingmagazine.com/the-death-of-the-blog-post/" target="_blank" class="screen">
+    <h3>Jason Gregory</h3>
+    <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/smashing.jpg" style="width: 460px; height: 6648px;" alt="" />
+  </a>
+</div>
+
+
+
+<div class="site">
+  <div class="description-wrap">
+    <div class="description">
+      <h2>Zoinks!</h2>        
       <p>What a great batch of inspiration. Now it's your turn!</p>
       
-      <p>Use this <a href="">wordpress theme framework</a> to make your very own Art-Directed Blaaag!</p>
+      <p>Get this <a href="https://github.com/travisneilson/Art-Directed" style="font-weight: bold;">Wordpress Theme Framework from github</a> and make your very own Art-Directed Blaaag today!</p>
             
       <ul class="inside-switch">
         <li class="prev">&#9664; Previous Blog</li>
@@ -252,7 +274,7 @@
   </div>
   
   <div class="screen">
-    <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/snap.png" style="width: 312px; height: 596px; box-shadow: none;" alt="" />
+    <img src="<?php bloginfo('template_url'); ?>/custom-posts/001-examples/img/octocat.jpg" style="width: 102px; height: 178px; box-shadow: none;" alt="" />
   </div>
   
 </div> 
